beabe84d696ad49efc1de51fc55cca85294ce631
Paldeep Singh <sasuk@DESKTOP-7TVM8OC.localdomain>
2022-09-17 13:10:24 +1000

    Add tests for adminUserExists function

--- src/__tests__/dynamoDB.test.ts
+++ src/__tests__/dynamoDB.test.ts
@@ -7,13 +7,33 @@ import {
 import * as dynamoDB from "../dynamoDB";
 
 describe("adminUserExists", () => {
-  describe("if the user does not exist", () => {
+  describe("if the admin user does not exist", () => {
     const email = faker.internet.email();
 
     it("returns false", async () => {
       const response = await dynamoDB.adminUserExists(email);
 
       expect(response).toBe(false);
+    });
+  });
+
+  describe("if the admin user exists", () => {
+    const email = faker.internet.email();
+
+    const adminUser = createAdminUser({ email });
+
+    beforeAll(async () => {
+      await insertTestAdminUser(adminUser);
+    });
+
+    afterAll(async () => {
+      await deleteTestAdminUser(email);
+    });
+
+    it("returns true", async () => {
+      const response = await dynamoDB.adminUserExists(email);
+
+      expect(response).toBe(true);
     });
   });
 });
