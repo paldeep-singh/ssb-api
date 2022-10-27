e13971d1093425c4ef3dd9341f41b5df6932e6e9
Paldeep Singh <paldeep@DESKTOP-7TVM8OC>
2022-10-27 20:41:19 +1100

    Add specific user authoriser function

--- src/functions/adminUser/authorisers.ts
+++ src/functions/adminUser/authorisers.ts
@@ -8,13 +8,28 @@ import { fetchSession } from './models/sessions'
 export const SPECIFIC_ADMIN_USER_AUTHORISER = 'specificAdminUserAuthoriser'
 export const SPECIFIC_ADMIN_USER_AUTHORISER_FUNCTION = `${SPECIFIC_ADMIN_USER_AUTHORISER}Function`
 
-const specificAdminUserAuthoriser: LambdaEventWithUnknownSchema<
-  boolean
-> = async (event): Promise<boolean> => {
+type IAuthoriserResult = {
+  isAuthorized: boolean
+  context?: Record<string, unknown>
+}
+
+const createAuthoriserResult = (
+  isAuthorized: boolean,
+  context?: Record<string, unknown>
+): IAuthoriserResult => {
+  return {
+    isAuthorized,
+    context
+  }
+}
+
+export const specificAdminUserAuthoriser: LambdaEventWithUnknownSchema<
+  IAuthoriserResult
+> = async (event) => {
   const { headers, body } = event
 
   if (!headers || !headers.Authorization) {
-    return Promise.resolve(false)
+    return createAuthoriserResult(false)
   }
 
   const sessionID = headers.Authorization
@@ -23,28 +38,28 @@ const specificAdminUserAuthoriser: LambdaEventWithUnknownSchema<
     typeof body === 'object' && !Array.isArray(body) && body !== null
 
   if (!bodyIsObject) {
-    return Promise.resolve(false)
+    return createAuthoriserResult(false)
   }
 
   const { email, userId } = body as Record<string, string>
 
   if (!email && !userId) {
-    return Promise.resolve(false)
+    return createAuthoriserResult(false)
   }
 
   const session = await fetchSession(sessionID)
 
-  if (!session) return Promise.resolve(false)
+  if (!session) return createAuthoriserResult(false)
 
   const {
     sessionData: { userId: sessionUserId }
   } = session
 
   if (userId) {
-    return Promise.resolve(userId === sessionUserId)
+    return createAuthoriserResult(userId === sessionUserId)
   }
 
   const adminUser = await fetchUser(userId)
 
-  return adminUser.email === email
+  return createAuthoriserResult(adminUser.email === email)
 }
