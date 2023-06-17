375fd97ee256032a60bcc0d5a7cdab2caec9844f
Paldeep <paldeepsingh21@gmail.com>
2023-06-17 17:46:07 +1000

    Mount separate import folder

---
+++ scripts/localDb.sh
@@ -0,0 +1,12 @@
+cp -r ./localRecipeDb/import ./localRecipeDb/neo4j/import || echo "Could not copy import folder, delete the folder if it already exists"
+
+docker compose -f localRecipeDb/docker-compose.recipe.db.yml up -d
+
+while ! curl http://localhost:7474/
+do
+  echo "$(date) - still trying"
+  sleep 1
+done
+echo "$(date) - connected successfully"
+
+docker exec --interactive recipe_db_local cypher-shell --file import/1.recipeImport.cypher -u neo4j -p pleaseletmein
