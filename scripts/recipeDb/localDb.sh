d58cdc830a531457cbe9298d37ca31a5a99e2434
Paldeep <paldeepsingh21@gmail.com>
2023-06-17 14:29:41 +1000

    Mostly working local recipe db script

--- scripts/recipeDb/localDb.sh
+++ scripts/recipeDb/localDb.sh
@@ -1,21 +1,11 @@
-container_id=$(docker run \
-        -it --rm --detach \
-        --publish=7474:7474 --publish=7687:7687 \
-        --volume=$HOME/neo4j/data:/data \
-        --volume=$PWD/import:/var/lib/neo4j/import \
-        --env=NEO4J_PLUGINS='["apoc"]' \
-        --env NEO4J_AUTH=none \
-        neo4j
-    )
 
-echo "Container ID: $container_id"
+docker compose -f scripts/recipeDb/docker-compose.recipe.db.yml up -d
 
-# docker cp ./import/1.recipeImport.cypher $container_id:/import/1.recipeImport.cypher
+while ! curl http://localhost:7474/
+do
+  echo "$(date) - still trying"
+  sleep 1
+done
+echo "$(date) - connected successfully"
 
-# docker exec $container_id cypher-shell --file /import/1.recipeImport.cypher
-
-# docker exec --interactive --tty $container_id \
-#     cypher-shell -u neo4j -p neo4j \
-#     --file /migrations/1.recipeImport.cypher
-
-docker stop $container_id
+docker exec --interactive recipe_db_local cypher-shell --file /var/lib/neo4j/import/1.recipeImport.cypher -u neo4j -p pleaseletmein
